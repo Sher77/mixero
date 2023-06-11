a93191ae3823c4c6d31efeadfde9dd0fef90917d
Sher77 <zaripovsherzat@mail.ru>
2023-06-11 16:59:42 +0600

    feat: add animations

--- js/script.js
+++ js/script.js
@@ -17,3 +17,22 @@ window.addEventListener('resize', function() {
 })
 
 
+const textWrapper = document.querySelector('.hero__description .letters');
+
+if(textWrapper) {
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+  anime.timeline({loop: true})
+    .add({
+      targets: '.hero__description .letter',
+      rotateY: [-90, 0],
+      duration: 700,
+      delay: (el, i) => 20 * i
+    }).add({
+      targets: '.hero__description',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
+}
